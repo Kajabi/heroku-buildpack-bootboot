eb3ae679fa9b1fac1d55c16eb51529a4dea3a126
Jacob Smith <jacob@jacobsmith.io>
2020-02-03 12:53:23 -0800

    Configure gems and specs

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source "https://rubygems.org"
+
+ruby "2.5.7"
+
+group :development, :test do
+  gem "heroku_hatchet"
+  gem "parallel_tests"
+  gem "rake"
+  gem "rspec"
+  gem "rspec-retry"
+  gem "rspec_junit_formatter"
+  gem "standard"
+  gem "toml-rb"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+require "rspec/core"
+require "rspec/retry"
+require "hatchet"
+require "language_pack/shell_helpers"
+
+ENV["RACK_ENV"] = "test"
+
+DEFAULT_STACK = "heroku-18"
+
+RSpec.configure do |config|
+  config.alias_example_to :fit, focused: true
+  config.filter_run_when_matching :focus unless ENV["CI"]
+
+  config.default_retry_count = 2 if ENV["CI"] # retry all tests that fail again
+  config.verbose_retry = true # show retry status in spec process
+
+  config.example_status_persistence_file_path = "tmp/examples.txt"
+  config.warnings = true
+
+  config.include LanguagePack::ShellHelpers
+end
+
+ReplRunner.register_commands(:console) do |config|
+  config.terminate_command "exit" # the command you use to end the 'rails console'
+  config.startup_timeout 60 # seconds to boot
+  config.return_char "\n" # the character that submits the command
+  config.sync_stdout "STDOUT.sync = true" # force REPL to not buffer standard out
+end
